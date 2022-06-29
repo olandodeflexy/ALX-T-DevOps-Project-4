cd0c2c57cf5e27665fe46e6ab907bb419e5940a8
Ubuntu <ubuntu@ip-172-31-15-117.ec2.internal>
2022-06-29 19:27:18 +0000

    Project 4

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=codesa/predictapp:v1.0.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run predict  --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward predict 8000:80
