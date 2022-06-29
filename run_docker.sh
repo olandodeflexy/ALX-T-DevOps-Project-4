46adf120a05312aca0ea1b20cd20c8c54d46a7eb
Ubuntu <ubuntu@ip-172-31-15-117.ec2.internal>
2022-06-29 18:51:36 +0000

    Project 4

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+sudo docker build --tag=predict .
 
 # Step 2: 
 # List docker images
+sudo docker images
 
 # Step 3: 
 # Run flask app
+sudo docker run -p 8000:80 predict 
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=codesa/predictapp:v1.0.0
 # Step 2:  
 # Authenticate & tag
+docker login
+docker image tag predict $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
